431486d2fa2c92caf9fa8dc5c0ed650a7c580b75
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-06 06:11:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,13 @@ class Song
 
   def self.genre_count
     genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] +=1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 
 end

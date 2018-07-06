29e94ff76fa13282245adf953039025498cee8c0
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-06 06:15:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -25,7 +25,8 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] +=1
+      if artist_count[artist]
+        artist_count[artist] += 1
       else
         artist_count[artist] = 1
       end
@@ -40,7 +41,8 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] +=1
+      if genre_count[genre]
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
